d1c31588713b11421d8b2dfc06bf2d8bea8864d9
visual studio <adityashiroor@gmail.com>
2022-02-13 13:37:09 +0530

    Added routes, and 404 page

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, RouteParams, RouterContext } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 const app = new Application();
 
 app.addEventListener("listen", ({ hostname, port, secure }) => {
@@ -22,15 +22,30 @@ app.use(async (ctx, next) => {
     }
 });
 
+const playground = async (ctx: RouterContext) => {
+    const playground = await Deno.readFile("./playground.html");
+    ctx.response.headers =
+        new Headers({
+            'Content-Type': 'text/html'
+        });
+    ctx.response.body = new TextDecoder('utf-8').decode(playground);
+};
+
+const router =
+    new Router()
+        .get('/play', playground)
+        .get('/playground', playground);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 app.use(async ctx => {
-    ctx.response.body = "404!";
+    const not_found = await Deno.readFile("./404.html");
+    ctx.response.headers =
+        new Headers({
+            'Content-Type': 'text/html'
+        });
+    ctx.response.body = new TextDecoder('utf-8').decode(not_found);
 });
-// const router =
-//     new Router()
-//         .get("/", ctx => {
-//             // do something
-//         });
-// app.use(router.routes());
-// app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
