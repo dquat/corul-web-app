39ee2f183d1343571a8d7e3310be709096849a78
visual studio <adityashiroor@gmail.com>
2022-02-16 20:53:11 +0530

    Added moving cursor, line highlight

--- public/playground.js
+++ public/playground.js
@@ -1,6 +1,16 @@
 // last 2 are depreciated, but if e is null, they can be used instead
 const get_e = e => e || window.event || event;
 
+const TEParams =  {
+    caret_class    : 'caret',
+    line_class     : 'code-line',
+    blink_interval : 500,
+    blink_class    : 'caret-blink',
+    focused_class  : 'editor-focus',
+    editor_class   : 'editor',
+    line_focused   : 'line-focus',
+}
+
 // WYSIWYG
 // the "hack" text editor, allows us to have full control making
 // code highlighting
@@ -11,52 +21,84 @@ const get_e = e => e || window.event || event;
 class TextEditor {
     constructor(
         el = document.body,
-        params = {
-            caret_class    : 'caret',
-            line_class     : 'code-line',
-            blink_interval : 500,
-            blink_class    : 'caret-blink',
-        }
+        params = TEParams
     ) {
         this.el = el;
         this.caret = document.createElement('div');
         if (params.caret_class)
             this.caret.classList.add(params.caret_class);
-        this.line_class  = '.' + (params.line_class || 'code-line');
-        this.blink_class = params.blink_class || 'caret-blink';
+        this.line_class  = '.' + (params.line_class || TEParams.line_class);
+        this.blink_class = params.blink_class || TEParams.blink_class;
         this.lines = [];
         this.blinker = null;
-        this.blink_interval = params.blink_interval ?? 500;
+        this.blink_interval = params.blink_interval ?? TEParams.blink_interval;
+        this.focus_class = params.focused_class || TEParams.focused_class;
+        this.editor_class = '.' + (params.editor_class || TEParams.editor_class);
+        this.line_focused = (params.line_focused || TEParams.line_focused);
+        this.prev_focus_line = null;
     }
 
     blink() {
         this.blinker = setInterval(() => {
-
             this.caret.classList.toggle(this.blink_class);
         }, this.blink_interval);
     }
 
-    det_closest_ln(_e) {
-        const e = get_e(_e);
+    get_closest_ln(e) {
         const cy = e.clientY || e.pageY || e.screenY;
         let closest = e.target.closest(this.line_class);
         if (!closest)
             for (const ln of this.lines) {
-                const top = ln.offsetTop;
+                const top = ln.getBoundingClientRect().top;
                 if (cy >= top) {
                     closest = ln;
                     break;
                 }
             }
         if (!closest)
-            if (cy < this.lines[this.lines.length - 1]?.offsetTop)
+            if (cy < this.lines[this.lines.length - 1]?.getBoundingClientRect().top)
                 closest = this.lines[this.lines.length - 1];
             else
                 closest = this.lines[0];
+        return closest;
+    }
+
+    set_curr_line(line) {
+        if (line && line !== this.prev_focus_line) {
+            for (const ln of this.lines)
+                ln.classList.remove(this.line_focused);
+            line.classList.add(this.line_focused);
+            this.prev_focus_line = line;
+        } else if (!line)
+            for (const ln of this.lines)
+                ln.classList.remove(this.line_focused);
+    }
+
+    move_caret(_e) {
+        const e       = get_e(_e),
+              closest = this.get_closest_ln(e);
         if (closest) {
-            const [ top, left ] = [ closest.offsetTop, closest.offsetLeft ];
-            this.caret.style.top  = `${top }px`;
-            this.caret.style.left = `${left}px`;
+            const bc = closest.getBoundingClientRect();
+            const [ top, left ] = [ bc.top, bc.left ];
+            this.caret.style.top = `${top}px`;
+            // use set timeout because selection api sucks and, selection does
+            // not get registered directly during mousedown
+            setTimeout(() => {
+                const sel = window.getSelection();
+                if (sel) {
+                    const node   = sel.anchorNode,
+                          offset = sel.anchorOffset;
+                    if (node) {
+                        let range = document.createRange();
+                        range.setStart(node, offset);
+                        range.setEnd  (node, offset);
+                        this.set_curr_line(node.parentElement?.closest(this.line_class));
+                        this.caret.style.left = range.getBoundingClientRect().left + "px";
+                    }
+                } else {
+                    this.caret.style.left = `${left}px`;
+                }
+            }, 1);
         }
     }
 
@@ -64,16 +106,38 @@ class TextEditor {
         let line_height =
             window.getComputedStyle(this.el)
                 .getPropertyValue('line-height');
-        this.el.style.setProperty('--caret-height', `${parseFloat(line_height) * 0.9}px`);
+        this.caret.style.setProperty('--caret-height', `${parseFloat(line_height) * 0.92}px`);
+    }
+
+    focus_events(focused) {
+        let display = 'block';
+        if (!focused) {
+            display = 'none';
+            this.set_curr_line(null);
+        }
+        this.caret.style.display = display;
+    }
+
+    toggle_focus(_e) {
+        const e = get_e(_e);
+        if (e.target.closest(this.editor_class)) {
+            this.el.classList.add(this.focus_class);
+            this.focus_events(true);
+        } else {
+            this.el.classList.remove(this.focus_class);
+            this.focus_events(false);
+        }
     }
 
     run() {
-        this.el.append(this.caret);
+        this.focus_events(false);
+        document.body.append(this.caret);
         this.lines.push(...this.el.querySelectorAll(this.line_class));
         this.lines = this.lines.reverse();
         this.update_line_height();
         this.blink();
-        document.addEventListener('mousedown', this.det_closest_ln.bind(this));
+        this.el.addEventListener('mousedown', this.move_caret.bind(this));
+        document.addEventListener('mousedown', this.toggle_focus.bind(this));
     }
 }
 
