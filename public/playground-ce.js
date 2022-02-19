8d2f1b8ec5241d2ef0425666ab43cc6bd62bb623
visual studio <adityashiroor@gmail.com>
2022-02-19 11:52:58 +0530

    Added 'README' in html & fixed some bugs

--- public/playground-ce.js
+++ public/playground-ce.js
@@ -1,4 +1,10 @@
+// About innerHTML, I know it's bad and all, and I *will remove* it later on, when I implement
+// an actual colorizing function. For now, deal with it. Or, don't visit this website :)
+
+const _is_chrome = !!window.chrome; // currently unused...
 const editor = document.querySelector('.editor');
+const highlights =
+    document.querySelectorAll('.highlight');
 
 function locate_cursor_pos(root, index) {
     // borrowed from SO answer: https://stackoverflow.com/a/38479462
@@ -7,7 +13,7 @@ function locate_cursor_pos(root, index) {
         document.createTreeWalker(root, NODE_TYPE, (elem) => {
             if (index > elem.textContent.length) {
                 index -= elem.textContent.length;
-                return NodeFilter.FILTER_REJECT
+                return NodeFilter.FILTER_REJECT;
             }
             return NodeFilter.FILTER_ACCEPT;
         });
@@ -17,55 +23,64 @@ function locate_cursor_pos(root, index) {
         position: index
     };
 }
-editor.addEventListener("input", e => {
+
+const colorize = input => {
+    return input
+        // '<' -> &lt; '>' -> &gt;
+        .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+        // strings
+        .replace(/"([^"]*)"?/gs, (s) => `<span class='string'>${s}</span>`)
+        // numbers
+        .replace(/\d+/g, (s) => `<span class='number'>${s}</span>`)
+        // multiline-comments
+        .replace(/\/\*(.*?)\*\//gs, (s) => `<span class='comment'>${s}</span>`)
+        // single line comments
+        .replace(/\/\/(.*)\n?/g, (s) => `<span class='comment'>${s}</span>`)
+        // some, not all special characters
+        .replace(/([\^\-|%!+(){}\[\].,?~]|&lt;|&gt;)/g, (s) => `<span class='special'>${s}</span>`)
+        // some keywords
+        .replace(/(fn|let|const|return|if|else|true|false|for|while|loop)/g,
+            (s) => `<span class='keyword'>${s}</span>`);
+}
+
+for (const highlighter of highlights)
+    highlighter.innerHTML = colorize(highlighter.textContent);
+
+const input = _ => {
     const sel = window.getSelection();
     const rng = sel.getRangeAt(0);
     const clone = rng.cloneRange();
     clone.setStart(editor, 0);
     const len = clone.toString().length;
+    let tc = editor.textContent;
     // test highlighting, a simple, regex based one, not the real one tho
-    992
     editor.innerHTML =
-        editor.innerText
-            .replace(/\d+/g, (s) => `<span style='color: #d3869b;'>${s}</span>`)
-            .replace(/(\/\*(.*)\*\/)|(\/\/(.*)\n)/g, (s) => `<span style='color: #928374;font-style: italic;'>${s}</span>`)
-            .replace(/(fn|let|const|return|if|else|true|false)/g, (s) => `<span style='color: #fe8019;font-style: italic;'>${s}</span>`);
+        colorize(editor.textContent)
+        // fixes bug in chromium (wants 2 '\n's at the end of input for 1 '\n')
+        // and maybe firefox bug too?...
+        + (tc.slice(-1) !== '\n' ? '\n' : '');
 
     let range = document.createRange();
     range.setStart(editor, 0);
-    let { node, position } = locate_cursor_pos(editor, len);
+    let {node, position} = locate_cursor_pos(editor, len);
     range.setStart(node, position);
     range.setEnd(node, position);
     range.collapse(true);
     sel.removeAllRanges();
     sel.addRange(range);
-});
+}
+
+editor.addEventListener("input", input);
 
 editor.addEventListener('keydown', e => {
     if (e.key === "Tab" || (e.keyCode || e.which) === 9) {
         e.preventDefault();
         const sel = window.getSelection();
         const rng = sel.getRangeAt(0);
-        const prev = rng.cloneRange();
-        prev.setStart(editor, 0);
-        const last = prev.toString().slice(-1);
-        const df = document.createDocumentFragment();
-        const el = document.createElement('span');
-        let tc = '';
-        if (last === '\n' && !!window.chrome) {
-            if (sel.anchorOffset === 0 && sel.anchorNode.previousSibling) {
-                let ps = sel.anchorNode.previousSibling;
-                while (ps?.textContent.length === 0)
-                    ps = ps?.previousSibling;
-                rng.setStart(ps, ps.textContent.length - 1)
-            } else
-                rng.setStart(sel.anchorNode, sel.anchorOffset - 1);
-            tc += '\n';
-        }
-        el.textContent = tc + '\t';
-        df.appendChild(el);
+        const s = document.createElement('span');
+        s.appendChild(document.createTextNode('\t'));
         rng.deleteContents();
-        rng.insertNode(df);
+        rng.insertNode(s);
         rng.collapse(false);
     }
 
@@ -74,25 +89,11 @@ editor.addEventListener('keydown', e => {
         e.preventDefault();
         const sel = window.getSelection();
         const rng = sel.getRangeAt(0);
-        const prev = rng.cloneRange();
-        prev.setStart(editor, 0);
-        const last = prev.toString().slice(-1);
         const df = document.createDocumentFragment();
-        const el = document.createElement('span');
-        console.log(sel, sel.anchorNode === editor.lastChild, sel.anchorOffset >= editor.lastChild.textContent.length)
-        el.textContent = '\n' +
-            (
-                // check to see if we're at the end of the input, and if the browser is chromium
-                // if it is, we add another newline, cuz for some reason chrome seems to want 2
-                // newline characters at the end of the input
-                (sel.anchorNode === editor.lastChild || sel.anchorNode === editor) &&
-                sel.anchorOffset >= sel.anchorNode.textContent.length && !!window.chrome
-                    ? '\n' : ''
-            );
-        df.appendChild(el);
+        df.appendChild(document.createTextNode('\n'));
         rng.deleteContents();
         rng.insertNode(df);
-        rng.setStartAfter(el);
         rng.collapse(false);
+        input(e); // cursor sometimes bounces when event is not triggered here
     }
-})
+});
